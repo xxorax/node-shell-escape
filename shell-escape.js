dcc1f27e5ae03948fe9dd34b27717e445c34b8ae
Michael Hurni <michael.hurni@gmail.com>
2015-03-31 09:55:06 +0200

    Make : and = non-wrapping arg into string
    useful for argument
    - Add some tests
    - Also make RegExp looking for at least one unsafe char instead of looking for full safe chars (more efficient with very huge string)
    - Bump version for npm update

--- shell-escape.js
+++ shell-escape.js
@@ -5,7 +5,7 @@ function shellescape(a) {
   var ret = [];
 
   a.forEach(function(s) {
-    if (!/^[A-Za-z0-9_\/-]+$/.test(s)) {
+    if (/[^A-Za-z0-9_\/:=-]/.test(s)) {
       s = "'"+s.replace(/'/g,"'\\''")+"'";
       s = s.replace(/^(?:'')+/g, '') // unduplicate single-quote at the beginning
         .replace(/\\'''/g, "\\'" ); // remove non-escaped single-quote if there are enclosed between 2 escaped
